7577d7354f13f6afb9900304702a562c6dc86ad9
DorianContal <dorian.contal@epitech.eu>
2023-10-27 17:07:07 +0200

    Exo8

--- SuperMutant.java
+++ SuperMutant.java
@@ -4,6 +4,25 @@ public class SuperMutant extends Monster {
     public SuperMutant() {
         super("SuperMutant #" + (++superMutantCount), 170, 20);
         System.out.println(getName() + ": Roaarrr!");
+        this.damage = 60;
+        this.ap = 20;
+    }
+
+    @Override
+    public void recoverAP() {
+        if (isDead == !true) {
+            if (ap < 50) {
+                ap += 7;
+                hp += 10;
+                if (ap > 50) {
+                    ap = 50;
+
+                }
+                if (hp > 170){
+                    hp = 170;
+                }
+            }
+        }
     }
 
 }
