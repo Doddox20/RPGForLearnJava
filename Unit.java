c20633e01f7bd2497af60e504dc7d60dd1611ab4
DorianContal <dorian.contal@epitech.eu>
2023-10-27 10:17:38 +0200

    hh

---
+++ Unit.java
@@ -0,0 +1,41 @@
+public abstract class Unit {
+    protected String name;
+    protected int hp;
+    protected int ap;
+
+    protected Unit(String name, int hp, int ap) {
+        this.name = name;
+        this.hp = hp;
+        this.ap = ap;
+    }
+
+    public int getAp() {
+        return ap;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void receiveDamage(int damage) {
+        if (hp > 0) {
+            hp -= damage;
+        }
+        if (hp <= 0) {
+            hp = 0;
+        }
+    }
+
+    public boolean moveCloseTo(Fighter target) {
+        if (target != this && target instanceof Fighter) {
+            System.out.println(this.getName() + " is moving closer to " + target.getName());
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
