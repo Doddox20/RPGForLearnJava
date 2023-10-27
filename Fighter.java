7f6e3ca3ebd507b022c87e8eac8582b2c9484f03
DorianContal <dorian.contal@epitech.eu>
2023-10-27 09:48:47 +0200

    Exo3

---
+++ Fighter.java
@@ -0,0 +1,19 @@
+public interface Fighter {
+
+
+    boolean equip(Weapon weapon);
+
+    boolean attack(Fighter target);
+
+    void receiveDamage(int damage);
+
+    boolean moveCloseTo(Fighter target);
+
+    void recoverAP();
+
+    String getName();
+
+    int getAP();
+    
+    int getHP();
+}
