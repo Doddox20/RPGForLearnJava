1a258c151a8023d8d811e2da8a4bd80bb6abfe68
DorianContal <dorian.contal@epitech.eu>
2023-10-27 10:48:51 +0200

    gg

--- Monster.java
+++ Monster.java
@@ -15,4 +15,20 @@ public abstract class Monster extends Unit{
     public void equip(){
         System.out.println("Monsters are proud and fight with their own bodies.");
     }
+
+    public boolean attack(Fighter target){
+        if (!moveCloseTo(target)) {
+            System.out.println(this.getName() + ": I'm too far away from " + target.getName());
+            return false;
+        }
+        if (this.getAp() > this.getApcost()){
+            System.out.println(this.getName() + " attacks " + target.getName());
+            ap -= apcost;
+            return true;
+        } else {
+
+        }
+
+    }
+
 }
